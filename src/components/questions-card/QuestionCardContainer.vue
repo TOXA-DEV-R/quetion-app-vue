b7f8aa600661514109ebd59957315e261d3a25c9
Tohir <TOXIR_DEV>
2022-08-24 23:12:05 +0600

    I'm creating a testing app.

---
+++ src/components/questions-card/QuestionCardContainer.vue
@@ -0,0 +1,34 @@
+<script lang="ts" >
+import { defineComponent, PropType, ref } from 'vue';
+import { mapState } from 'vuex';
+import { QuestionListTypes } from '../../types/index';
+
+export default defineComponent({
+  name: "Card-container",
+  props: {
+    data: {
+      type: Object as PropType<QuestionListTypes[]>,
+      required: true,
+    }
+  }
+})
+</script>
+
+<template>
+  <div class="card mt-4">
+    <div class="card-header">
+      <h5 class="card-title">
+        50.Choose the appropriate answer for the definition below: the act of brining goods , letters, etc to a
+        particular person or place, or the things that are brought
+      </h5>
+    </div>
+    <div class="card-body list-group p-0">
+      <li class="list-group-item rounded-0 border-0">An item</li>
+    </div>
+    <div class="card-footer d-flex flex-wrap justify-content-between">
+      <button class="btn btn-primary text-uppercase">Previous</button>
+      <button class="btn btn-primary text-uppercase">Submit</button>
+      <button class="btn btn-primary text-uppercase">next</button>
+    </div>
+  </div>
+</template>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,70 @@
 /** @format */
 
-import { createStore, Store } from "vuex";
+import { ActionPayload, createStore, Store } from "vuex";
 import { InjectionKey } from "vue";
-import { CategoryListTypes, StateTypes } from "../types";
-import { getCategories } from "../api/index";
+import { QuestionListTypes, StateTypes, GetQuestionTypes } from "../types";
+import { getQuestionListApi } from "../api/index";
 
 const key: InjectionKey<Store<StateTypes>> = Symbol();
 
 const ActionKeys = {
-  GETCATEGORYLIST: "GETCATEGORYLIST",
+  GETQUESTIONLIST: "GETQUESTIONLIST",
 };
-const {} = ActionKeys;
+const { GETQUESTIONLIST } = ActionKeys;
 
 const store = createStore<StateTypes>({
-  state: {},
-  mutations: {
-    GETCATEGORYLIST({}, payload) {},
+  state: {
+    questionList: [],
+    questionListLength: 0,
+    isFinishedQuestions: false,
+    controlQuestionNavigatorCurrent: 0,
+    thingsOfNavbar: {
+      isOpen: false,
+    },
   },
-  actions: {},
+  mutations: {
+    [GETQUESTIONLIST](state, payload) {
+      function frmatData(payload: any[]): QuestionListTypes[] {
+        const data: QuestionListTypes[] = [];
+
+        for (let key in payload) {
+          const { correct_answer, question, incorrect_answers } = payload[key];
+
+          data.push({
+            correct_answer,
+            question,
+            id: Number(key),
+            incorrect_answers: (incorrect_answers as string[]).map(
+              (item, indx) => ({
+                value: item,
+                id: indx,
+              })
+            ),
+          });
+        }
+        return data;
+      }
+      state.questionList = frmatData(payload);
+      console.log(state.questionList);
+      state.questionListLength = payload?.length;
+    },
+    openThingsOfNavbar(state) {
+      state.thingsOfNavbar.isOpen = true;
+    },
+    closeThingsOfNavbar(state) {
+      state.thingsOfNavbar.isOpen = false;
+    },
+  },
+  actions: {
+    async getQuestionList(
+      { commit },
+      { amount, category }: { amount: number; category: number }
+    ) {
+      const response = await getQuestionListApi(amount, category);
+      const results: GetQuestionTypes[] = response?.results;
+      commit(GETQUESTIONLIST, results);
+    },
+  },
   getters: {},
 });
 
